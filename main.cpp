328a89e340c2ac7d77e3428dd963a04299a045ed
suminskiwiktor <245933@edu.p.lodz.pl>
2022-11-03 15:33:33 +0100

    dodano policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+int policz(int a, int b){
+    return a+b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout<<"Policz 2+3="<<policz(2,3)<<std::endl;
     return 0;
 }
